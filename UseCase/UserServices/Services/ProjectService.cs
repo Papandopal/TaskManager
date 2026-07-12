a6532b5a0edee477ff48aba50d90db0ee70786b2
Papandopal <obezan8@gmail.com>
2026-07-12 14:45:50 +0300

    fix: rename AuthorisationServices to UserServices

---
+++ UseCase/UserServices/Services/ProjectService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase.AuthorisationServices.Services
+{
+    public class ProjectService
+    {
+        public void CreateProject
+    }
+}
